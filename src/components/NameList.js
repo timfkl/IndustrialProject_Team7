215e78eadb8290c5b856db6450ff8bd795891b35
FreetimeFM <awesomea380@tutanota.com>
2021-09-30 13:42:28 +0100

    Formatted document

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,20 +2,19 @@ import React from "react";
 import { ListGroup } from "react-bootstrap";
 
 // Takes in an array of names as input.
-const NameList = ({list}) => {
-
-  // Each element in array is mapped to an individual list group item.
-  return (
-    <ListGroup>
-      {list.map((name, index) => {
-        return (
-          <ListGroup.Item action key={index}>
-            {name}
-          </ListGroup.Item>
-        );
-      })}
-    </ListGroup>
-  );
-}
+const NameList = ({ list }) => {
+    // Each element in array is mapped to an individual list group item.
+    return (
+        <ListGroup>
+            {list.map((name, index) => {
+                return (
+                    <ListGroup.Item action key={index}>
+                        {name}
+                    </ListGroup.Item>
+                );
+            })}
+        </ListGroup>
+    );
+};
 
 export default NameList;
--- src/pages/AthleteList.js
+++ src/pages/AthleteList.js
@@ -3,35 +3,35 @@ import NameList from "../components/NameList";
 
 const AthleteList = () => {
 
-  // Test name array. Will be replaced with database call. 
-  const list = [
-    "Geoffrey Gould",
-    "Caroline Pena",
-    "Felipe Cooley",
-    "Queen York",
-    "Bennett Koch",
-    "Kristofer Villanueva",
-    "Ivan Logan",
-    "Antwan Watkins",
-    "Dong Conner",
-    "Aldi Lidl"
-  ]
+    // Test name array. Will be replaced with database call. 
+    const list = [
+        "Geoffrey Gould",
+        "Caroline Pena",
+        "Felipe Cooley",
+        "Queen York",
+        "Bennett Koch",
+        "Kristofer Villanueva",
+        "Ivan Logan",
+        "Antwan Watkins",
+        "Dong Conner",
+        "Aldi Lidl"
+    ]
 
-  return (
-    <Container>
+    return (
+        <Container>
 
-      <h3>List of Athletes</h3>
+            <h3>List of Athletes</h3>
 
-      <h6>
-        {list.length > 0 ? "Please choose an athlete" : "You don't have any athletes to review."}
-      </h6>
+            <h6>
+                {list.length > 0 ? "Please choose an athlete" : "You don't have any athletes to review."}
+            </h6>
 
-      <div>
-        <NameList list={list} />
-      </div>
-      
-    </Container>
-  );
+            <div>
+                <NameList list={list} />
+            </div>
+
+        </Container>
+    );
 }
 
 export default AthleteList;
