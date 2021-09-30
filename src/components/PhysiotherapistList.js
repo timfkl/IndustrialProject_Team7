ae5e24f9ab605332ab2f8ccf54b4b8d2a4808f2d
FreetimeFM <awesomea380@tutanota.com>
2021-09-30 10:11:17 +0100

    Mapped list group with custom array

--- src/components/PhysiotherapistList.js
+++ src/components/PhysiotherapistList.js
@@ -1,10 +1,31 @@
 import { ListGroup } from "react-bootstrap";
 
 function PhysiotherapistList() {
+  const nameList = [
+    "Moshe Chapman",
+    "Dustin Evans",
+    "Jasper Levine",
+    "Octavio Powell",
+    "Silas Henry",
+    "Brennen Boyle",
+    "Krish Booth",
+    "Frederick Brown",
+    "Koen Massey",
+    "Ronan Dixon",
+    "Araceli Gibbs",
+    "Aldi Lidl",
+  ];
 
   return (
 
     <ListGroup>
+      {nameList.map((name, index) => {
+        return (
+          <ListGroup.Item action key={index} href={index}>
+            {name}
+          </ListGroup.Item>
+        );
+      })}
     </ListGroup>
   );
 }
