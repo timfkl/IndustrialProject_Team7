d9e412ed3598011fcf473a876d64e3a7ab022a16
FreetimeFM <awesomea380@tutanota.com>
2021-09-30 13:08:43 +0100

    Imported namelist

--- src/pages/AthleteList.js
+++ src/pages/AthleteList.js
@@ -1,9 +1,14 @@
-
+import NameList from "../components/NameList";
 
 const AthleteList = () => {
 
+  const list = [
+    "test"
+  ]
 
-
+  return (
+    <NameList list={list} />
+  );
 }
 
 export default AthleteList;
