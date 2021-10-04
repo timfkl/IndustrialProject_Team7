0d41d8fcd3c2576124cf61505910bc61d93b89f2
FreetimeFM <awesomea380@tutanota.com>
2021-10-04 19:16:35 +0100

    Added backdrop images

--- src/components/TwoDHeatmap.js
+++ src/components/TwoDHeatmap.js
@@ -1,8 +1,25 @@
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import imgFront from '../assets/front.jpg';
+import imgBack from '../assets/back.jpg';
+const DataImage = ({ id, src, onLoad }) => {
+    return (
+        <div id={id}>
+            <img src={src} alt="" width="100%" style={{ zIndex: -1 }} onLoad={onLoad} />
+        </div>
+    );
+}
 const TwoDHeatmap = () => {
     return (
 
         <Container>
+            <Row>
+                <Col sm={6}>
+                    <DataImage id={"imageFront"} src={imgFront} onLoad={() => { onDataImageLoad(true) }} />
+                </Col>
+                <Col sm={6}>
+                    <DataImage id={"imageBack"} src={imgBack} onLoad={() => { onDataImageLoad(false) }} />
+                </Col>
+            </Row>
         </Container>
 
     );
