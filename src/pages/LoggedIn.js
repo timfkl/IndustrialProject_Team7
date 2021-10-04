091b5be248963b743714cca723a49703b55a1a9a
FreetimeFM <awesomea380@tutanota.com>
2021-10-04 19:21:08 +0100

    Add 2D heatmap to page

--- src/pages/LoggedIn.js
+++ src/pages/LoggedIn.js
@@ -1,25 +1,31 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 //for styling 
 import styled from 'styled-components';
 import Common from '../components/Common';
 
+import { Container, Row, Col } from 'react-bootstrap';
+import TwoDHeatmap from '../components/TwoDHeatmap';
 import CSVUploadButton from '../components/CSVUploadButton';
 
 const LoggedIn = () => {
+
     return (
-       
         <header> 
-             <div className="Container">
-            <h5>Athlete View</h5>
-            <p>View your latest session here</p>
+            <div className="Container">
+                <h5>Athlete View</h5>
+                <p>View your latest session here</p>
+                
                 <Container>
+                    <TwoDHeatmap />
                     <Row>
                         <Col>
                             <CSVUploadButton/>
                         </Col>
                     </Row>
                 </Container>
+
+                <p> stats on the session </p>
+                <StyledLink className="LinkContain" to="/progress"> Progress </StyledLink> 
             </div>
         </header>
 
