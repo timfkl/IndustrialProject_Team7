c085cb9a4c066598d5726f184b1be9a274ceb1f2
FreetimeFM <awesomea380@tutanota.com>
2021-10-04 19:15:55 +0100

    Added TwoDHeatmap file

---
+++ src/components/TwoDHeatmap.js
@@ -0,0 +1,11 @@
+import { Container, Row, Col, Button } from 'react-bootstrap';
+const TwoDHeatmap = () => {
+    return (
+
+        <Container>
+        </Container>
+
+    );
+}
+
+export default TwoDHeatmap;
