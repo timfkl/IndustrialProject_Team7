8c1f03829e207c8b7680dba585b70120c5bf83c0
FreetimeFM <awesomea380@tutanota.com>
2021-10-04 19:17:44 +0100

    Create instances of heatmaps when backdrops load

--- src/components/TwoDHeatmap.js
+++ src/components/TwoDHeatmap.js
@@ -1,6 +1,8 @@
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import imgFront from '../assets/front.jpg';
 import imgBack from '../assets/back.jpg';
+import h337 from 'heatmap.js';
+
 const DataImage = ({ id, src, onLoad }) => {
     return (
         <div id={id}>
@@ -8,7 +10,40 @@ const DataImage = ({ id, src, onLoad }) => {
         </div>
     );
 }
+
 const TwoDHeatmap = () => {
+    let heatmapQuadLeft, heatmapQuadRight, heatmapHamsLeft, heatmapHamsRight;
+
+    const onDataImageLoad = (isFront) => {
+        
+        if (isFront) {
+            heatmapQuadLeft = createHeatmapInstance('imageFront');
+            heatmapQuadRight = createHeatmapInstance('imageFront');
+        } else {
+            heatmapHamsLeft = createHeatmapInstance('imageBack');
+            heatmapHamsRight = createHeatmapInstance('imageBack');
+        }
+    }
+
+    const createHeatmapInstance = (container) => {
+                
+        return h337.create({
+            // only container is required, the rest will be defaults
+            container: document.getElementById(container),
+            blur: 0,
+            radius: 20,
+            minOpacity: 0.7,
+            gradient: {
+                '0': 'green',
+                '0.2': '#EDD74B',
+                '0.4': '#F2B62E',
+                '0.6': '#DB8535',
+                '0.8': '#F25B2E',
+                '0.95': '#E82C3D'
+            }
+        });
+    }
+
     return (
 
         <Container>
