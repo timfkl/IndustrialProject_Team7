63e30034cae0acc1d06b9c09f7255968d4fd81b9
FreetimeFM <awesomea380@tutanota.com>
2021-10-06 17:02:17 +0100

    Formatted Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,67 +1,65 @@
-import React, { useState } from 'react';
-import LoginForm from '../components/LoginForm';
+import React, { useState } from "react";
+import LoginForm from "../components/LoginForm";
 
 //stylised login page
 const Login = () => {
     const adminUser = {
         email: "test@test.com", //hard codded login details.
-        password: "test123"
-      }
-    
-      const [user, setUser] = useState({email: "", password: ""});
-      const [error, setError] = useState("");//seter for errors not in use. Could add catch errors if needed.
-      
-      const Login = details => {
+        password: "test123",
+    };
+
+    const [user, setUser] = useState({ email: "", password: "" });
+    const [error, setError] = useState(""); //seter for errors not in use. Could add catch errors if needed.
+
+    const Login = (details) => {
         console.log(details);
-    
-        if(details.email == adminUser.email && details.password == adminUser.password) { //if the submitted details of the login match, call setter to update details.
-          console.log("User Logged In");
-          setUser({
-            email: details.name,
-            password: details.password
-          });
+
+        if (details.email == adminUser.email && details.password == adminUser.password) {
+            //if the submitted details of the login match, call setter to update details.
+            console.log("User Logged In");
+            setUser({
+                email: details.name,
+                password: details.password,
+            });
         } else {
-          console.log("Wrong details");
-          alert("Wrong Details");
+            console.log("Wrong details");
+            alert("Wrong Details");
         }
-    
-      }
-    
-      const Logout = () => { //If logout button is clicked, set the login details to blank and reload the page. Results starting back at login page.
+    };
+
+    const Logout = () => {
+        //If logout button is clicked, set the login details to blank and reload the page. Results starting back at login page.
         console.log("User Logged Out");
         setUser({
-          email: "",
-          password: ""
+            email: "",
+            password: "",
         });
         window.location.reload();
-      }
-    
-      return ( //Split login page into 3 cols in order to center login and maybe add content to the side if needed later.
-        <div className="LoginPage">
-          <div className="container">
-              <div className = "row">
-                <div className = "col-lg-4">
-                </div>
-                <div className="col-lg-4">
-                {(user.email != "") ? ( //If the email is not blank (the user has succesfully submitted the correct details), change the elements on the page to display the welcome page. Might be simpler to take user to new html file instead.
-                    <div className = "welcome">
-                      <h2>Welcome bro, <span>{user.name}</span></h2>
-                      <button onClick={Logout}>Logout</button>
-                    </div>
-                  ) : (
-                    <LoginForm Login={Login} error={error}/>
-                  )}
-              </div>
-              <div className="col-lg-4">
-    
-              </div>
-              </div>
-            </div>
-          </div>
-    
-    
-    
-      );
-}
+    };
 
-export default Login
+    return (
+        //Split login page into 3 cols in order to center login and maybe add content to the side if needed later.
+        <div className="LoginPage">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-4"></div>
+                    <div className="col-lg-4">
+                        {user.email != "" ? ( //If the email is not blank (the user has succesfully submitted the correct details), change the elements on the page to display the welcome page. Might be simpler to take user to new html file instead.
+                            <div className="welcome">
+                                <h2>
+                                    Welcome bro, <span>{user.name}</span>
+                                </h2>
+                                <button onClick={Logout}>Logout</button>
+                            </div>
+                        ) : (
+                            <LoginForm Login={Login} error={error} />
+                        )}
+                    </div>
+                    <div className="col-lg-4"></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
