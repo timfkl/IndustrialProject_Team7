b31f0929368560ea7dd20287cdeb7cbcd4928ad2
Kemian-JMO <55388741+Kemian-JMO@users.noreply.github.com>
2021-10-07 13:11:37 +0100

    Update connection.php

--- public/api/connection.php
+++ public/api/connection.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: http://localhost:3000');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 
