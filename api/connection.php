21a4bfeef4d2b1ec06d2d9879c34ddff6b78ca44
Kemian-JMO <55388741+Kemian-JMO@users.noreply.github.com>
2021-10-07 12:41:43 +0100

    Update connection.php

--- api/connection.php
+++ api/connection.php
@@ -5,7 +5,7 @@ header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 
 class Connection {
 	public function getConnection(){
-		
+
 		$db = "api";
 		$dbhost = "silva.computing.dundee.ac.uk";
 		$dbuser = "21ac4u07";
@@ -13,8 +13,8 @@ class Connection {
 		$dbname = "21ac4d07";
 		$dboptions = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
-		$db = new PDO("mysql:host=$dbhost;dbname=$db;", $dbuser, $dbpass, $dboptions);
-			return $db;
+		$con = mysqli_connect("silva.computing.dundee.ac.uk","21ac4u07","ab123c","21ac4d07");
+			return $con;
 	}
 }
 
