573fa3723ec1c56c35b99102a026d70083b4ac8e
Kemian-JMO <55388741+Kemian-JMO@users.noreply.github.com>
2021-10-07 11:05:01 +0100

    Create index.php

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+	$_SESSION;
+
+?>
+<html>
+	<head>
+		<title>My website</title>
+	</head>
+	<body>
+
+		<a href="logout.php">Logout</a>
+		<h1>index yo</h1>
+
+
+	</body>
+</html>
