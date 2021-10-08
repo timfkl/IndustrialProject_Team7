c9caa14cc74d943251c1f317b94930866234ca33
FreetimeFM <awesomea380@tutanota.com>
2021-10-08 11:50:18 +0100

    Handle user sign out

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -3,10 +3,21 @@ import { Theme } from '../scripts/theme';
 
 // Custom login button component.
 const LoginButton = () => {
+
+    const handleClick = () => {
+        
+        if (localStorage.getItem('user_name')) {
+            localStorage.removeItem('user_name');
+            localStorage.removeItem('user_type_ID');
+            // localStorage.clear()
+        }
+    }
+
     return (
         <Button
             href="/login"
             variant="warning"
+            onClick={handleClick}
             style={{
                 backgroundColor: Theme.colors.orange,
                 borderColor: Theme.colors.orange,
