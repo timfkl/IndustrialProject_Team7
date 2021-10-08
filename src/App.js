5f70961cbbd4663c1f34f28e9ac370011fe878b2
FreetimeFM <awesomea380@tutanota.com>
2021-10-08 12:11:03 +0100

    Added comments to app.js

--- src/App.js
+++ src/App.js
@@ -11,15 +11,16 @@ import Navbar from "./components/NavigationBar"; //navbar component
 
 // Allows the user to move between the pages.
 function App() {
-
     return (
         <Router>
             <Navbar />
             <Switch>
+                {/* Normal paths that the public can access */}
                 <Route path="/about" component={About} />
                 <Route path="/contact" component={Contact} />
                 <Route path="/login" component={Login} />
-                
+
+                {/* Only logged in users can access */}
                 <PrivateRoute path="/loggedin" component={LoggedIn} />
                 <PrivateRoute path="/progress" component={Progress} />
                 <Route component={About} />
@@ -28,12 +29,14 @@ function App() {
     );
 }
 
+// Creates a route so that only logged in users can access them.
 const PrivateRoute = (props) => {
     const location = useLocation();
     const authLogin = localStorage.getItem('user_name');
     console.log("authLogin", authLogin);
     console.log(useLocation());
-  
+
+    // If user is logged in go to page specifed or else redirect to login page.
     return authLogin ? (
       <Route {...props} />
     ) : (
