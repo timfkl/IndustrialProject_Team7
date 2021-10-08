6f06ff9e1e8c809b754165d127ee948e2c5afea4
FreetimeFM <awesomea380@tutanota.com>
2021-10-08 15:24:12 +0100

    Added muscle selection

--- src/components/CSVUploadButton.js
+++ src/components/CSVUploadButton.js
@@ -2,12 +2,13 @@
 import './CSVUploadButton.css';
 import { useState } from 'react'
 import React from "react";
-import Modal from "react-bootstrap/Modal";
+import { Modal, Form } from "react-bootstrap";
 
 export default function CSVUploadButton(){
 
     // Create a state for the modal, telling whether it's open or closed
     const [isOpen, setIsOpen] = React.useState(false);
+    const [selectValue, setSelectValue] = React.useState(1);
     
     var muscleGroup
 
@@ -29,7 +30,7 @@ export default function CSVUploadButton(){
         const file = csvFile;
         const reader = new FileReader();
 
-        muscleGroup = document.getElementById("muscleGroup").value;
+        muscleGroup = selectValue;
 
         reader.onload = function(e) {
             const text = e.target.result;
@@ -45,46 +46,59 @@ export default function CSVUploadButton(){
     // The html of the component
     return (
         <>
+            {/* This button opens the modal to upload csv files */}
+            <button id="uploadButton" className="button" onClick={showModal}>
+                Upload a CSV
+            </button>
 
-        {/* This button opens the modal to upload csv files */}
-        <button id="uploadButton" className= "button" onClick={showModal}>Upload a CSV</button>
+            {/* When the modal setIsOpen is true, the modal is displayed and offers and area for users to upload a csv */}
+            <Modal show={isOpen} onHide={hideModal}>
+                <Modal.Header>
+                    <Modal.Title>Upload a .csv with sensor data here:</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {/* Area to upload csv */}
+                    <form id="csvForm">
+                        <input
+                            type="file"
+                            accept=".csv"
+                            id="csvFile"
+                            onChange={(e) => {
+                                setCsvFile(e.target.files[0]);
+                            }}
+                        ></input>
+                    </form>
+                </Modal.Body>
 
-        {/* When the modal setIsOpen is true, the modal is displayed and offers and area for users to upload a csv */}
-        <Modal show={isOpen} onHide={hideModal}>
-            <Modal.Header>
-                <Modal.Title>Upload a .csv with sensor data here:</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                {/* Area to upload csv */}
-                <form id='csvForm'>
-                    <input type='file' accept='.csv' id='csvFile' 
-                    onChange={(e) => {
-                        setCsvFile(e.target.files[0])
-                    }}
+                <Modal.Header>
+                    <Modal.Title>Which muscle is this data for?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Select onChange={(e) => {setSelectValue(e); console.log(e.target.value)}}>
+                        <option value={1}>Left Quadriceps</option>
+                        <option value={2}>Right Quadriceps</option>
+                        <option value={3}>Left Hamstring</option>
+                        <option value={4}>Right Hamstring</option>
+                    </Form.Select>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    {/* Buttons to close the modal, and submit it (which runs the submit method) */}
+                    <button id="cancelButton" className="button" onClick={hideModal}>
+                        Cancel
+                    </button>
+                    <button
+                        id="uploadButton"
+                        className="button"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            if (csvFile) submit();
+                        }}
                     >
-                    </input>
-                </form>
-            </Modal.Body>
-
-            <Modal.Header>
-                <Modal.Title>Which muscle group is this for? (1-4)</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <input type="text" id="muscleGroup" name="muscleGroup" placeholder= "e.g. 1" /><br/>
-            </Modal.Body>
-
-            <Modal.Footer>
-                {/* Buttons to close the modal, and submit it (which runs the submit method) */}
-                <button id="cancelButton"  className= "button" onClick={hideModal}>Cancel</button>
-                <button id="uploadButton"  className= "button"
-                    onClick={(e) => {
-                        e.preventDefault()
-                        if(csvFile)submit()
-                    }}>
-                    Submit 
-                </button>
-            </Modal.Footer>
-        </Modal>
+                        Submit
+                    </button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
 }
