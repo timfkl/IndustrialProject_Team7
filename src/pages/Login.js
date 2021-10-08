44de2fa653abd6e8dececab43230c08aa43f08d4
FreetimeFM <awesomea380@tutanota.com>
2021-10-08 12:04:05 +0100

    Formatted login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Alert, Container, Form, FloatingLabel, Button } from "react-bootstrap";
 import axios from "axios";
-// import './Login.css';
 
 //stylised login page
 const Login = () => {
@@ -11,33 +10,33 @@ const Login = () => {
     const API_PATH = "http://localhost/projects/api/login.php";
 
     // If the user is already logged in send them to the dashboard.
-    if (localStorage.getItem('user_name')) {
+    if (localStorage.getItem("user_name")) {
         window.location.href = "/loggedin";
     }
 
     //hard codded login details.
     const users = {
         physio: {
-            name: 'Darlene Alderson',
+            name: "Darlene Alderson",
             email: "physio@theohealth.com",
             password: "physio123",
-            userTypeID: 1
+            userTypeID: 1,
         },
         injured: {
-            name: 'Philip Price',
+            name: "Philip Price",
             email: "injured@theohealth.com",
             password: "injured123",
-            userTypeID: 2
+            userTypeID: 2,
         },
-        athlete:{
-            name: 'Fernando Vera',
+        athlete: {
+            name: "Fernando Vera",
             email: "athlete@theohealth.com",
             password: "athlete123",
-            userTypeID: 3
-        }
+            userTypeID: 3,
+        },
     };
 
-    const handleSubmit = e => {
+    const handleSubmit = (e) => {
         //Handles form submission. Prevents page from reloading in order to save the data. Then passes the details through to Login func.
         e.preventDefault();
 
@@ -67,40 +66,75 @@ const Login = () => {
             }
         }
         setError("The details you have entered are incorrect.");
-    }
+    };
 
     return (
         //Login form.
         <Container>
-            <Alert variant="danger" style={{ display: error !== "" ? "block" : "none", margin: 10, textAlign: 'center' }} onClose={() => {setError("")}} dismissible>
+            <Alert
+                variant="danger"
+                style={{
+                    display: error !== "" ? "block" : "none",
+                    margin: 10,
+                    textAlign: "center",
+                }}
+                onClose={() => {
+                    setError("");
+                }}
+                dismissible
+            >
                 {error}
             </Alert>
 
-            <div className="position-absolute top-50 start-50 translate-middle" style={{ minWidth: '30%', maxWidth: '90%', padding: '2rem', backgroundColor: 'white', borderRadius: 10, textAlign: 'center' }}>
-                <img src={`${process.env.PUBLIC_URL}/logo_name.png`} alt="" className="mb-3" style={{ minWidth: '30%', maxWidth: '90%' }} />
+            <div
+                className="position-absolute top-50 start-50 translate-middle"
+                style={{
+                    minWidth: "30%",
+                    maxWidth: "90%",
+                    padding: "2rem",
+                    backgroundColor: "white",
+                    borderRadius: 10,
+                    textAlign: "center",
+                }}
+            >
+                <img
+                    src={`${process.env.PUBLIC_URL}/logo_name.png`}
+                    alt=""
+                    className="mb-3"
+                    style={{ minWidth: "30%", maxWidth: "90%" }}
+                />
                 <h3 className="mb-5">Sign into your account</h3>
                 <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3"></Form.Group>
                     <FloatingLabel label="Email address" className="mb-3">
-                        <Form.Control name="email" type="email" placeholder="name@example.com" onChange={e => { setEmail(e.target.value) }} />
+                        <Form.Control
+                            name="email"
+                            type="email"
+                            placeholder="name@example.com"
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                            }}
+                        />
                     </FloatingLabel>
                     <Form.Group className="mb-3">
                         <FloatingLabel label="Password">
-                            <Form.Control name="password" type="password" placeholder="Password" onChange={e => { setPassword(e.target.value) }} />
+                            <Form.Control
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                onChange={(e) => {
+                                    setPassword(e.target.value);
+                                }}
+                            />
                         </FloatingLabel>
                     </Form.Group>
-                    <Button variant="warning" type="submit" >
+                    <Button variant="warning" type="submit">
                         Sign in
                     </Button>
                 </Form>
-
             </div>
-
         </Container>
     );
 };
 
 export default Login;
-
-
-// action={API_PATH} method="POST"
