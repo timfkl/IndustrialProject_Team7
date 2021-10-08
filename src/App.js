2174913cb6a80087178a61eafa78daf3b9dbaa70
FreetimeFM <awesomea380@tutanota.com>
2021-10-08 11:51:51 +0100

    Implemented protected pages if user is logged in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect, useLocation } from "react-router-dom";
 //import pages to link to
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
@@ -11,6 +11,7 @@ import Navbar from "./components/NavigationBar"; //navbar component
 
 // Allows the user to move between the pages.
 function App() {
+
     return (
         <Router>
             <Navbar />
@@ -18,11 +19,31 @@ function App() {
                 <Route path="/about" component={About} />
                 <Route path="/contact" component={Contact} />
                 <Route path="/login" component={Login} />
-                <Route path="/loggedin" component={LoggedIn} />
-                <Route path="/progress" component={Progress} />
+                
+                <PrivateRoute path="/loggedin" component={LoggedIn} />
+                <PrivateRoute path="/progress" component={Progress} />
+                <Route component={About} />
             </Switch>
         </Router>
     );
 }
+
+const PrivateRoute = (props) => {
+    const location = useLocation();
+    const authLogin = localStorage.getItem('user_name');
+    console.log("authLogin", authLogin);
+    console.log(useLocation());
+  
+    return authLogin ? (
+      <Route {...props} />
+    ) : (
+      <Redirect
+        to={{
+          pathname: "/login",
+          state: { from: location }
+        }}
+      />
+    );
+  };
 
 export default App;
