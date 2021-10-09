352a7101d9147eaa6679eb611f15abba905253d7
FreetimeFM <awesomea380@tutanota.com>
2021-10-09 19:08:07 +0100

    Refactored LoginButton.js to use OrangeButton.js

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,5 +1,4 @@
-import { Button } from 'react-bootstrap';
-import { Theme } from '../scripts/theme';
+import OrangeButton from './OrangeButton';
 import { useLocation } from 'react-router';
 
 // Custom login button component.
@@ -17,20 +16,13 @@ const LoginButton = () => {
     }
 
     return (
-        <Button
+        <OrangeButton
             href="/login"
-            variant="warning"
             onClick={handleClick}
-            style={{
-                backgroundColor: Theme.colors.orange,
-                borderColor: Theme.colors.orange,
-                color: 'white'
-            }}
             // disable button if user is already on the login page
             disabled={useLocation().pathname === '/login'}
-        >
-            { localStorage.getItem('user_name') ? 'Sign out' : 'Sign in' }
-        </Button>
+            text={ localStorage.getItem('user_name') ? 'Sign out' : 'Sign in' }
+        />
     );
 }
 
