019de154dcaa2d163a34da4018d42ae17ea997c4
FreetimeFM <awesomea380@tutanota.com>
2021-10-09 18:37:13 +0100

    renamed var from 'userButton' to 'dashboardButton'

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -6,7 +6,7 @@ import TealButton from '../components/TealButton';
 // Displays navigation bar.
 const NavigationBar = () => {
 
-    const userButton = <TealButton text="Dashboard" props={{ className: "me-1", href: "/dashboard" }} />
+    const dashboardButton = <TealButton text="Dashboard" props={{ className: "me-1", href: "/dashboard" }} />
 
     return (
         <Navbar bg="light" expand="sm">
@@ -32,7 +32,7 @@ const NavigationBar = () => {
                         {/* <Nav.Link href="/about">About</Nav.Link> */}
                         <Nav.Link href="/contact">Contact</Nav.Link>
                     </Nav>
-                    { localStorage.getItem('user_name') ? userButton : null }
+                    { localStorage.getItem('user_name') ? dashboardButton : null }
                     <LoginButton/>
                 </Navbar.Collapse>
             </Container>
