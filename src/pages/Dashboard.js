a3f8d92eb5537de40168d1152bd1776e1db29275
FreetimeFM <awesomea380@tutanota.com>
2021-10-10 10:31:58 +0100

    Fixed dashboard redirect error

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -10,7 +10,7 @@ const AthleteDashboard = () => {
 
     return (
         <Container className="mt-3">
-            <h2>Hello, {localStorage.getItem("user_name")}</h2>
+            <h2>Hello, {localStorage.getItem("user_name")}.</h2>
             <p>Welcome to your dashboard.</p>
             <Tabs className="mb-3" activeKey={tab} onSelect={(e) => setTab(e)}>
                 <Tab eventKey={1} title="Latest Session">
