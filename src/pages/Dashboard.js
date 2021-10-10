9d712ec718a082a23368457be5cc37c50b66cbe8
FreetimeFM <awesomea380@tutanota.com>
2021-10-10 10:09:28 +0100

    Split dashboard into 2 dashboards (physio and athlete)

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,9 +5,38 @@ import TwoDHeatmap from '../components/TwoDHeatmap'; // Contains 2D heatmap.
 import CSVUploadButton from '../components/CSVUploadButton'; // Csv upload button component.
 import Progress from './Progress';
 
-const Dashboard = () => {
-    const [tab, setTab] = useState(localStorage.getItem('user_type_ID') === '1' ? 1 : 2);
-    const PhysioTab = <Tab eventKey={1} title="Athlete List"><AthleteList/></Tab>
+const AthleteDashboard = () => {
+    const [tab, setTab] = useState(1);
+
+    return (
+        <Container className="mt-3">
+            <h2>Hello, {localStorage.getItem("user_name")}</h2>
+            <p>Welcome to your dashboard.</p>
+            <Tabs className="mb-3" activeKey={tab} onSelect={(e) => setTab(e)}>
+                <Tab eventKey={1} title="Latest Session">
+                    {/* Container for 2D Heatmap */}
+                    <TwoDHeatmap />
+                    <Row>
+                        <Col>
+                            <CSVUploadButton />
+                        </Col>
+                    </Row>
+                </Tab>
+                
+                <Tab eventKey={2} title="Progress">
+                    <Progress />
+                </Tab>
+                {/* <Tab eventKey={3} title="Edit Goals">
+                    <h3>Other Stuff to match wireframe</h3>
+                </Tab> */}
+            </Tabs>
+        </Container>
+    );
+}
+
+const PhysioDashboard = () => {
+    const [tab, setTab] = useState(1);
+    const [client, setClient] = useState("");
 
     return (
         <Container className="mt-3">
@@ -15,9 +44,11 @@ const Dashboard = () => {
             <p>Welcome to your dashboard.</p>
             <Tabs className="mb-3" activeKey={tab} onSelect={(e) => setTab(e)}>
                 
-                { localStorage.getItem('user_type_ID') === '1' ? PhysioTab : null }
+                <Tab eventKey={1} title="Athlete List">
+                    <AthleteList/>
+                </Tab>
 
-                <Tab eventKey={2} title="Latest Session" className="border-end">
+                <Tab eventKey={2} title="Latest Session">
                     {/* Container for 2D Heatmap */}
                     <TwoDHeatmap />
                     <Row>
@@ -36,6 +67,13 @@ const Dashboard = () => {
             </Tabs>
         </Container>
     );
+}
+
+const Dashboard = () => {
+
+    if (localStorage.getItem('user_type_ID') === '1') return <PhysioDashboard/>
+    else return <AthleteDashboard/>
+
 }
 
 export default Dashboard;
