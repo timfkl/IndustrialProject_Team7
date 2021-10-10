ee198720ecac9e85dd01d9e3cdf0c0086d4da019
FreetimeFM <awesomea380@tutanota.com>
2021-10-10 10:09:52 +0100

    Added prop for function onNameChosen

--- src/components/AthleteList.js
+++ src/components/AthleteList.js
@@ -1,10 +1,11 @@
 import { Container } from "react-bootstrap";
 import NameList from "./NameList";
 
-const AthleteList = () => {
+const AthleteList = ({ onNameChosen }) => {
 
     // Test name array. Will be replaced with database call. 
     const list = [
+        "Philip Price",
         "Geoffrey Gould",
         "Caroline Pena",
         "Felipe Cooley",
@@ -17,16 +18,12 @@ const AthleteList = () => {
         "Aldi Lidl"
     ];
 
-    const handleItemClick = (name, index) => {
-        console.log(name);
-    }
-
     return (
         <Container className="mt-3">
             <h6>
                 {list.length > 0 ? "Please choose a client from the list" : "You don't have any athletes to review."}
             </h6>
-            <NameList list={list} onItmeClick={handleItemClick} />
+            <NameList list={list} onItemClick={onNameChosen} />
 
         </Container>
     );
