951a09b5cd58064758db5138fe6418ff08022b06
FreetimeFM <awesomea380@tutanota.com>
2021-10-10 11:43:06 +0100

    Added orange button color on mouseover

--- src/components/OrangeButton.js
+++ src/components/OrangeButton.js
@@ -1,18 +1,32 @@
 import { Button } from 'react-bootstrap';
 import { Theme } from '../scripts/theme';
+import { useState } from 'react';
 
 // Custom login button component.
 const OrangeButton = (props) => {
+    const [backColor, setBackColor] = useState(Theme.colors.orange);
+
+    const handleMouseEnter = e => {
+        setBackColor(Theme.mouseOver.orange);
+        if (props.onMouseEnter) props.onMouseEnter(e);
+    }
+
+    const handleMouseLeave = e => {
+        setBackColor(Theme.colors.orange);
+        if (props.onMouseLeave) props.onMouseLeave(e);
+    }
 
     return (
         <Button
             {...props}
             style={{
-                backgroundColor: Theme.colors.orange,
-                borderColor: Theme.colors.orange,
+                backgroundColor: backColor,
+                border: 0,
                 color: 'white'
             }}
             variant="warning"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
         >
             {props.text}
         </Button>
--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -2,5 +2,9 @@ export const Theme = {
     colors: {
         teal: '#0297a2',
         orange: '#f36d21'
+    },
+    mouseOver: {
+        teal: '',
+        orange: '#c75a1c'
     }
 }
