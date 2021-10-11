be9c92289b4c47b1f9bd1895f33cd3b7c539da4e
FreetimeFM <awesomea380@tutanota.com>
2021-10-11 22:05:36 +0100

    Use cards instead of list to display clients

--- src/components/AthleteList.js
+++ src/components/AthleteList.js
@@ -5,17 +5,51 @@ const AthleteList = ({ onNameChosen }) => {
 
     // Test name array. Will be replaced with database call. 
     const list = [
-        "Philip Price",
-        "Geoffrey Gould",
-        "Caroline Pena",
-        "Felipe Cooley",
-        "Queen York",
-        "Bennett Koch",
-        "Kristofer Villanueva",
-        "Ivan Logan",
-        "Antwan Watkins",
-        "Dong Conner",
-        "Aldi Lidl"
+        {
+            name: "Philip Price",
+            image: "https://i.pravatar.cc/150?u=PhilipPrice",
+            lastActive: "3 days ago",
+        },
+        {
+            name: "Geoffrey Gould",
+            image: "https://i.pravatar.cc/150?u=GeoffreyGould",
+            lastActive: "1 day ago",
+        },
+        {
+            name: "Caroline Pena",
+            image: "https://i.pravatar.cc/150?u=CarolinePena",
+            lastActive: "7 days ago",
+        },
+        {
+            name: "Felipe Cooley",
+            image: "https://i.pravatar.cc/150?u=FelipeCooley",
+            lastActive: "5 days ago",
+        },
+        {
+            name: "Queen York",
+            image: "https://i.pravatar.cc/150?u=QueenYork",
+            lastActive: "4 days ago",
+        },
+        {
+            name: "Bennett Koch",
+            image: "https://i.pravatar.cc/150?u=BennettKoch",
+            lastActive: "less than a day ago",
+        },
+        {
+            name: "Kristofer Villanueva",
+            image: "https://i.pravatar.cc/150?u=KristoferVillanueva",
+            lastActive: "less than a day ago",
+        },
+        {
+            name: "Ivan Logan",
+            image: "https://i.pravatar.cc/150?u=IvanLogan",
+            lastActive: "2 days ago",
+        },
+        {
+            name: "Antwan Watkins",
+            image: "https://i.pravatar.cc/150?u=AntwanWatkins",
+            lastActive: "1 day ago",
+        }
     ];
 
     return (
--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,20 +1,28 @@
 import React from "react";
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, Row, Col, Card } from "react-bootstrap";
 
 // Takes in an array of names as input.
 const NameList = ({ list, onItemClick }) => {
     // Each element in array is mapped to an individual list group item.
 
     return (
-        <ListGroup>
-            {list.map((name, index) => {
-                return (
-                    <ListGroup.Item action key={index} onClick={() => {onItemClick(name, index)}}>
-                        {name}
-                    </ListGroup.Item>
-                );
-            })}
-        </ListGroup>
+        <Row xs={2} sm={3} md={4} lg={5} className="g-4">
+            {list.map((item, index) => (
+                <Col>
+                    <Card bg="light">
+                        <Card.Img variant="top" src={item.image} />
+                        <Card.Body>
+                            <Card.Title>{item.name}</Card.Title>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroup.Item action>Select Client</ListGroup.Item>
+                            <ListGroup.Item action>See Details</ListGroup.Item>
+                        </ListGroup>
+                        <Card.Footer>Last active: {item.lastActive}</Card.Footer>
+                    </Card>
+                </Col>
+            ))}
+        </Row>
     );
 };
 
