a6ffc92736f9521aed1a420f7b83917cc852625d
FreetimeFM <awesomea380@tutanota.com>
2021-10-12 10:51:50 +0100

    Add links to cards in namelist

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,21 +2,21 @@ import React from "react";
 import { ListGroup, Row, Col, Card } from "react-bootstrap";
 
 // Takes in an array of names as input.
-const NameList = ({ list, onItemClick }) => {
+const NameList = ({ list, onItemClick, onDetailsClick }) => {
     // Each element in array is mapped to an individual list group item.
 
     return (
         <Row xs={2} sm={3} md={4} lg={5} className="g-4">
             {list.map((item, index) => (
-                <Col>
+                <Col key={index}>
                     <Card bg="light">
                         <Card.Img variant="top" src={item.image} />
                         <Card.Body>
                             <Card.Title>{item.name}</Card.Title>
                         </Card.Body>
                         <ListGroup className="list-group-flush">
-                            <ListGroup.Item action>Select Client</ListGroup.Item>
-                            <ListGroup.Item action>See Details</ListGroup.Item>
+                            <ListGroup.Item onClick={() => {onItemClick(item)}} action>Select Client</ListGroup.Item>
+                            <ListGroup.Item onClick={() => {onDetailsClick(item)}} action>See Details</ListGroup.Item>
                         </ListGroup>
                         <Card.Footer>Last active: {item.lastActive}</Card.Footer>
                     </Card>
