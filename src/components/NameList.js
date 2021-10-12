68a173ddf6d6600f6dade3364b157239c38ff555
FreetimeFM <awesomea380@tutanota.com>
2021-10-12 11:58:59 +0100

    Added comments to namelist

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { ListGroup, Row, Col, Card } from "react-bootstrap";
 
-// Takes in an array of names as input.
+// Takes a list of users as input, onItemClick is a function prop which runs when the user
+// clicks to see the athlete. onDetailsClick is a function prop which runs whern the user
+// wants to see athlete's details.
 const NameList = ({ list, onItemClick, onDetailsClick }) => {
-    // Each element in array is mapped to an individual list group item.
-
+    // Each element in array is mapped to an individual card containing links and details.
     return (
         <Row xs={2} sm={3} md={4} lg={5} className="g-4">
             {list.map((item, index) => (
