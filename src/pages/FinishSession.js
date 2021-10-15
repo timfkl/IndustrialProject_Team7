876077643ef252c35c3e2b8ed1d444ec59419dba
FreetimeFM <awesomea380@tutanota.com>
2021-10-15 13:10:18 +0100

    Fix simulation not running

--- src/pages/FinishSession.js
+++ src/pages/FinishSession.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import CSVToArray from "../scripts/CSVToArray";
 import { changeData } from '../components/LineChartWellBeing';
 import TealButton from '../components/TealButton';
 
